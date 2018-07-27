919b0ee177134028c9c4ac0789d98d2a81b36463
Yan Kaled <yan.kaled.barbosa@gmail.com>
2018-07-27 20:40:33 -0300

    Second github deploy

--- ethereum/gvcoin.js
+++ ethereum/gvcoin.js
@@ -3,7 +3,7 @@ import compiledGvCoin from './build/GvCoin.json';
 
 const instance = new web3.eth.Contract(
   JSON.parse(compiledGvCoin.interface),
-  '0x41Aa37ea9CfAe660965D801b26227B19233c6360'
+  process.env.CO
 );
 
 export default instance;
--- ethereum/web3.js
+++ ethereum/web3.js
@@ -8,7 +8,7 @@ if (typeof window !== 'undefined' && typeof window.web3 !== 'undefined') {
 } else {
   // We are on the server *OR* the user is not running metamask
   const provider = new Web3.providers.HttpProvider(
-    'https://rinkeby.infura.io/v3/7c5f1d509fe64238befbf589c3a386ed'
+    process.eventNames.INF
   );
   web3 = new Web3(provider);
 }
